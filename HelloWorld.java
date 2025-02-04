6b31be2d87d18bce34c290de0515a4dfc7e350c7
Qianyi Li <qli003@student.sdccd.edu>
2025-02-04 12:15:54 -0800

    Initial sharing of project

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+*This is my first Java program in CISC190 It writes out "Hello World" and my name
+*
+* @author Qianyi Li
+* @version v1.0
+* @since 2/4/2025
+*/
+public class HelloWorld
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Hello World");
+        System.out.println("Qianyi Li");
+    }
+}
+
